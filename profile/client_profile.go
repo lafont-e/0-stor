b364cb3408505d53705df3cf9c92ec1cb53cfead
katia-e <ekaterina_tut@inbox.ru>
2017-12-04 18:41:54 +0100

    Adds main

---
+++ profile/client_profile.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/pkg/profile"
+)
+
+var (
+	profileOutDir = flag.String("profileOutDir", "", "")
+	profileMode   = flag.String("profile.mode", "", "enable profiling mode, one of [cpu, mem, trace, block]")
+)
+
+func main() {
+	flag.Parse()
+
+	switch *profileMode {
+	case "cpu":
+		defer profile.Start(profile.ProfilePath(*profileOutDir), profile.CPUProfile).Stop()
+	case "mem":
+		defer profile.Start(profile.ProfilePath(*profileOutDir), profile.MemProfile).Stop()
+	case "trace":
+		defer profile.Start(profile.ProfilePath(*profileOutDir), profile.TraceProfile).Stop()
+	case "block":
+		defer profile.Start(profile.ProfilePath(*profileOutDir), profile.BlockProfile).Stop()
+	default:
+		return
+	}
+
+	// code for client
+}
